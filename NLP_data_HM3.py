c0b0980eed898f9549fe5c6fcab0eef969a67f8a
nothiefhouse <fforner@stevens.edu>
2021-04-03 16:58:29 -0400

    Update NLP_data_HM3.py

--- NLP_data_HM3.py
+++ NLP_data_HM3.py
@@ -16,11 +16,11 @@ sentiment = find_HM3.find_sent(Purpose) # find sentiment
 df1["NLTK_Sentiment"] = sentiment[0]
 NLFK_top_best = df1.sort_values(by='NLTK_Sentiment', ascending=False).head(10)
 NLFK_top_worst = df1.sort_values(by='NLTK_Sentiment', ascending=True).head(10)
-print (f'\n\n\nThis first analysis using the NLTK sentiment analysis method.\nThe best ten positive result are:\n{NLFK_top_best} \n\n\nThe worst ten result are:\n{NLFK_top_worst} ')
+print (f'\n\n\nThis is the first analysis using the NLTK sentiment analysis method.\nThe best ten positive result are:\n{NLFK_top_best} \n\n\nThe worst ten result are:\n{NLFK_top_worst} ')
 
 
 ## BlobTEXT
 df2["BlobText_sentiment"] = sentiment[1]
 BlobTEXT_top_best = df2.sort_values(by='BlobText_sentiment', ascending=False).head(10)
 BlobTEXT_top_worst = df2.sort_values(by='BlobText_sentiment', ascending=True).head(10)
-print (f'\n\n\nThis first analysis using the NLTK sentiment analysis method.\nThe best ten positive result are:\n{BlobTEXT_top_best} \n\n\nThe worst ten result are:\n{BlobTEXT_top_worst} ')
+print (f'\n\n\nThis is the second analysis using the NLTK sentiment analysis method.\nThe best ten positive result are:\n{BlobTEXT_top_best} \n\n\nThe worst ten result are:\n{BlobTEXT_top_worst} ')
